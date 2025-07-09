4c834ea9a5a47dc6f2094ce58fc245954f9b05bf
YoussefAmrAbdelmoneim <yosefamr200@gmail.com>
2025-07-09 13:33:35 +0300

    Demo Video02

--- Demo Session 4/Program.cs
+++ Demo Session 4/Program.cs
@@ -61,7 +61,56 @@
             //        break;
             #endregion
             #endregion
-
+            #region Video 02
+            #region Ex1 Pattern matching without alias name
+            //object number = 5.6;
+            //switch (number)
+            //{
+            //    case int:
+            //        Console.WriteLine("Int");
+            //        break;
+            //    case decimal:
+            //        Console.WriteLine("Decimal");
+            //         break;
+            //    case double when (double) number >5.5 && (double) number <10.10:
+            //        Console.WriteLine("Double");
+            //        break;
+            //}
+            #endregion
+            #region Ex2 Switch expression
+            //Console.WriteLine("Enter Your number:");
+            //string option = Console.ReadLine() ?? "0";
+            //string message;
+            //message = option switch
+            //{
+            //    "1" => "Option 1",
+            //    "2" => "Option 2",
+            //    "3" => "Option 3",
+            //    _ => "Invalid Option"
+            //};
+            //Console.WriteLine(message);
+            #endregion
+            #region Ex3 Property pattern
+            //Person person = new Person() { Id = 10, Name = "Omar", Age = 25 };
+            //string message = person switch
+            //{
+            //    {Name:"Ahmed",Age:10} => "Hello Ahmed",
+            //    {Name:"Omar"} => "Hello Omar",
+            //    { Id:50} =>"Hello person with Id = 20",
+            //    _ => "Sorry we don't know you"
+            //};
+            #endregion
+            #region Ex4 Nullable Type
+            //int? number = 10;
+            //string Result = number switch
+            //{
+            //    null => "Nullable Type",
+            //    int x when x >0 => "Positive number",
+            //    int x when x < 0 => "Negative number",
+            //     0 => "Zero"
+            //};
+            #endregion
+            #endregion
         }
     }
 }
