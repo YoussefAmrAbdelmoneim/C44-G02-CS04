59eee7bb0b768dde2ff7ec9db53550b2d7ea7dcd
YoussefAmrAbdelmoneim <yosefamr200@gmail.com>
2025-07-09 11:27:18 +0300

    Demo Video01

--- Demo Session 4/Program.cs
+++ Demo Session 4/Program.cs
@@ -4,7 +4,64 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Video 01
+            //object number = 5.2;
+            #region Ex1 [Pattern Matching on Types]
+            //switch (number)
+            //{
+            //    case int value:
+            //        Console.WriteLine("Integer");
+            //        break;
+            //    case decimal value:
+            //        Console.WriteLine("decimal");
+            //        break;
+            //    case double value:
+            //        Console.WriteLine("Double");
+            //        break;
+            //    default:
+            //        Console.WriteLine("No Matching");
+            //        break;
+            //}
+            #endregion
+            #region EX2 [Case Guards [When]]
+            //switch (number)
+            //{
+            //    case int value when value <10:
+            //        Console.WriteLine("Integer is less than 10");
+            //        break;
+            //    case int value when value > 10:
+            //        Console.WriteLine("Integer is more than 10");
+            //        break;
+            //    case decimal value:
+            //        Console.WriteLine("decimal");
+            //        break;
+            //    case double value when value > 5.5 && value < 10.10:
+            //        Console.WriteLine("Double is between 5.5 and 10.10");
+            //        break;
+            //    default:
+            //        Console.WriteLine("No Matching");
+            //        break;
+            //}
+            #endregion
+            #region EX3 [User Defined Data Type]
+            //object input = 60;
+            //input = new Person(Id = 1, Name = "Omar", Age = 25);
+            //switch(input) {
+            //    case int value when value >10:
+            //        Console.WriteLine("Int greater than 10");
+            //        break;
+            //    case string value:
+            //        Console.WriteLine("String");
+            //        break;
+            //    case Person value when value.Id == 1 && value.Name == "Omar":
+            //        Console.WriteLine("Person");
+            //        break;
+            //    default:
+            //        Console.WriteLine("No Matching");
+            //        break;
+            #endregion
+            #endregion
+
         }
     }
 }
